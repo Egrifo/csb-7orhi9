5345864b0f000756930ecbacc81c1ce830b37934
Code Sandbox <hello@codesandbox.io>
2022-09-09 16:46:42 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.253c956299e2e7f6b1007b2c99686a03.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76c692fa6011856409f53a31b2b13e55",
+    "url": "https://egrifo.github.io/csb-7orhi9/index.html"
+  },
+  {
+    "revision": "0f60cae44efcbec6b9e1",
+    "url": "https://egrifo.github.io/csb-7orhi9/static/js/2.3e9548b7.chunk.js"
+  },
+  {
+    "revision": "3d6e51f3fbaaf640d0ea",
+    "url": "https://egrifo.github.io/csb-7orhi9/static/js/main.37c06d74.chunk.js"
+  },
+  {
+    "revision": "390054ffa9c69441b7cb",
+    "url": "https://egrifo.github.io/csb-7orhi9/static/js/runtime~main.b24b5afb.js"
+  }
+]);
